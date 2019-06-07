cfb07b65bc68dfdae1698501cc919d1c1c355658
Anit Kumar <anikumarsingh99@gmail.com>
2019-06-07 10:54:49 +0530

    renaming

--- src/routes/AppRoutes.js
+++ src/routes/AppRoutes.js
@@ -2,7 +2,7 @@ import React,{ Component } from 'react';
 import { Route , Switch } from 'react-router-dom';
 import Home from '../pages/Home';
 import WelcomeScreen from '../pages/LoginAuth/Welcome';
-import Error from '../pages/Error/Error';
+import ErrorPage from '../pages/Error/Error';
 import DragNdrop from '../pages/DragNDrop/'
 
 class App extends Component {
@@ -12,10 +12,10 @@ class App extends Component {
           <Switch>
             { sessionStorage.getItem("userData")
              ?(<Route path={`${match.path}home`} component={Home} />)
-             :(<Route path={`${match.path}home`} component={Error}/>)}
+             :(<Route path={`${match.path}home`} component={ErrorPage}/>)}
             {sessionStorage.getItem("userData")
             ?(<Route path={`${match.path}drag-n-drop`} component={DragNdrop} />)
-            :(<Route path={`${match.path}drag-n-drop`} component={Error} />)}
+            :(<Route path={`${match.path}drag-n-drop`} component={ErrorPage} />)}
             <Route path={`${match.path}/`} component={WelcomeScreen}/>
           </Switch>
       );
