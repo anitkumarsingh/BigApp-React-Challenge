fa85a64c3f177469d076690d74d911841c720a9f
Anit Kumar <anikumarsingh99@gmail.com>
2019-06-10 21:45:01 +0530

    new approach for protected routing

--- src/routes/AppRoutes.js
+++ src/routes/AppRoutes.js
@@ -3,19 +3,34 @@ import { Route , Switch } from 'react-router-dom';
 import Home from '../pages/Home';
 import WelcomeScreen from '../pages/LoginAuth/Welcome';
 import ErrorPage from '../pages/Error/Error';
-import DragNdrop from '../pages/DragNDrop/'
+import DragNdrop from '../pages/DragNDrop/';
+
+const PrivateRoute = ({ component: Component, ...rest }) => (
+  <Route
+    {...rest}
+    render={props => {
+      return sessionStorage.getItem("userData") ? (
+        <Component {...props} />
+      ) : (
+          <ErrorPage {...props}  />
+        );
+    }}
+  />
+);
 
 class App extends Component {
     render() {
       const { match } = this.props;
          return (
           <Switch>
-            { sessionStorage.getItem("userData")
-             ?(<Route path={`${match.path}home`} component={Home} />)
-             :(<Route path={`${match.path}home`} component={ErrorPage}/>)}
-            {sessionStorage.getItem("userData")
-            ?(<Route path={`${match.path}drag-n-drop`} component={DragNdrop} />)
-            :(<Route path={`${match.path}drag-n-drop`} component={ErrorPage} />)}
+             <PrivateRoute
+              path={`${match.path}home`}
+              component={Home}
+             />
+           <PrivateRoute
+              path={`${match.path}drag-n-drop`}
+              component={DragNdrop}
+            />
             <Route path={`${match.path}/`} component={WelcomeScreen}/>
           </Switch>
       );
