59ada98038aa8aeafccc8dc400d66d20e2b63193
Anit Kumar <anikumarsingh99@gmail.com>
2019-06-14 00:15:55 +0530

    hashrouting

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 import NavBar from './components/NavBar/NavBar';
 import Footer from './components/Footer/Footer';
 import OuterFooter from'./components/Footer/OuterFooter';
